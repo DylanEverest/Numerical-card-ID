725d0de1cf24523d08985b6913bcf8d99020c20c
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-15 18:26:00 +0300

    Info model

---
+++ Health-services/HealthApplication/Models/Information.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthApplication.Models
+{
+    public class Info
+    {
+        public string CardId { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+
+}
