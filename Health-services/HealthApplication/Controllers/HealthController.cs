f661c94b9506b17e2e7c5e38d2caac6231c7d9ee
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-15 19:22:35 +0300

    controllers

--- Health-services/HealthApplication/Controllers/HealthController.cs
+++ Health-services/HealthApplication/Controllers/HealthController.cs
@@ -1,24 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using System.Data;
-using System.Data.SqlClient; // Ou tout autre fournisseur de base de données pris en charge par Dapper
-using Dapper;
 using HealthApplication.Models;
+using HealthApplication.Repository;
 
 namespace HealthApplication.Controllers
 {
+    [Route("api/health")]
     [ApiController]
-    [Route("[controller]")]
     public class HealthController : ControllerBase
     {
-        private readonly IDbConnection _connection;
+        private readonly HealthRepository _repository;
 
-        public HealthController(IDbConnection connection)
+        public HealthController(HealthRepository repository)
         {
-            _connection = connection;
+            _repository = repository;
         }
 
         [HttpGet("{cardId}")]
@@ -26,18 +22,28 @@ namespace HealthApplication.Controllers
         {
             try
             {
-                HealthInf health = _connection.QueryFirstOrDefault<HealthInf>(
-                    "SELECT * FROM health WHERE card_id = @CardId",
-                    new { CardId = cardId }
-                );
+                IEnumerable<HealthInf> health = _repository.GetHealthByCardId(cardId);
+                return Ok(health);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
 
+        [HttpGet("byId/{healthId}")]
+        public IActionResult GetHealthById(int healthId)
+        {
+            try
+            {
+                HealthInf health = _repository.GetHealthById(healthId);
                 if (health != null)
                 {
                     return Ok(health);
                 }
                 else
                 {
-                    return NotFound();
+                    return NotFound("Santé introuvable.");
                 }
             }
             catch (Exception ex)
@@ -46,5 +52,63 @@ namespace HealthApplication.Controllers
             }
         }
 
+        [HttpPost]
+        public IActionResult CreateHealth([FromBody] HealthInf health)
+        {
+            try
+            {
+                _repository.CreateHealth(health);
+                return CreatedAtAction(nameof(GetHealthById), new { healthId = health.HealthId }, health);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{healthId}")]
+        public IActionResult UpdateHealth(int healthId, [FromBody] HealthInf updatedHealth)
+        {
+            try
+            {
+                HealthInf existingHealth = _repository.GetHealthById(healthId);
+                if (existingHealth == null)
+                {
+                    return NotFound("Santé introuvable.");
+                }
+
+                existingHealth.CardId = updatedHealth.CardId;
+                existingHealth.Diseases = updatedHealth.Diseases;
+                existingHealth.DateBegin = updatedHealth.DateBegin;
+                existingHealth.DateEnd = updatedHealth.DateEnd;
+
+                _repository.UpdateHealth(existingHealth);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{healthId}")]
+        public IActionResult DeleteHealth(int healthId)
+        {
+            try
+            {
+                HealthInf existingHealth = _repository.GetHealthById(healthId);
+                if (existingHealth == null)
+                {
+                    return NotFound("Santé introuvable.");
+                }
+
+                _repository.DeleteHealth(healthId);
+                return Ok("Santé supprimée.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
