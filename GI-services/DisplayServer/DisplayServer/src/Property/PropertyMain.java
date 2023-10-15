58288f05feb2ed89a4051fa6d1b45a169ebbebdf
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-15 23:58:16 +0300

    refactor

--- GI-services/DisplayServer/DisplayServer/src/Property/PropertyMain.java
+++ GI-services/DisplayServer/DisplayServer/src/Property/PropertyMain.java
@@ -1,11 +1,8 @@
 package Property;
 
-import Jersey.Post;
-import Property.response.PropertyResponse;
 
 public class PropertyMain {
     public static void main(String[] args) throws Exception {
-
         Post<PropertyResponse> ps = new Post<PropertyResponse>("http://dylan-aspireek571g:8080/Property/resources/property/getByNIC") ;
         PropertyResponse obj = new PropertyResponse();
         obj.setCardId("12345abc");
