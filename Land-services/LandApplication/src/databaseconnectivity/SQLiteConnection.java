b09ce523da202d23de2dba97de1ba572ea88ea33
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-15 16:17:16 +0300

    SQLiteConnection

---
+++ Land-services/LandApplication/src/databaseconnectivity/SQLiteConnection.java
@@ -0,0 +1,39 @@
+// /home/dylan/Desktop/SQLite
+package databaseconnectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteConnection {
+    private String database;
+    private Connection connection;
+
+    public SQLiteConnection(String database) {
+        this.database = database;
+    }
+
+    public Connection connectToDatabase() throws SQLException {
+        if (connection != null) {
+            return connection; // Utiliser la connexion existante si elle existe.
+        }
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String url = "jdbc:sqlite:" + database;
+            connection = DriverManager.getConnection(url);
+            return connection;
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Le pilote JDBC SQLite n'a pas pu être chargé.");
+        }
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+}
+
