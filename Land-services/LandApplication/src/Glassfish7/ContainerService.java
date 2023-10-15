5dd015c3ad00279438db056dba80f6ef19a92c7b
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-15 17:20:35 +0300

    modified:   Land-services/LandApplication/bin/Glassfish7/ContainerService.class 	modified:   Land-services/LandApplication/src/Glassfish7/ContainerService.java

--- Land-services/LandApplication/src/Glassfish7/ContainerService.java
+++ Land-services/LandApplication/src/Glassfish7/ContainerService.java
@@ -3,6 +3,7 @@ package Glassfish7;
 import java.util.HashSet;
 import java.util.Set;
 
+import controller.PropertyController;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
@@ -15,6 +16,8 @@ public class ContainerService extends Application
         Set<Class<?>> s = new HashSet<Class<?>>();
         // s.add(ExempleResource.class);
         // s.add(Employee.class);
+        s.add(PropertyController.class);
+        
         return s;
     }
     
