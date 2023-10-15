1bab3f52b713075ff39e147861a227cfab7da236
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-15 18:22:58 +0300

    modified:   Health-services/ScriptSQL/Health-schema.sql

--- Health-services/ScriptSQL/Health-schema.sql
+++ Health-services/ScriptSQL/Health-schema.sql
@@ -1,9 +1,19 @@
 create database health_check;
 
+create table infos(
+    card_id varchar(50) primary key,
+    name varchar(20),
+    firstname varchar(20),
+    birthdate date
+);
+
+
 create table health(
     health_id serial primary key,
-    card_id varchar(50),
+    card_id varchar(50) references infos (card_id),
     deseases varchar ,
     date_begin date not null ,
     date_end date
 );
+
+
