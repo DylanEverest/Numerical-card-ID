07b7fd7af2cfb9bb43d360647f7a82de8c2eb17b
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-16 11:00:25 +0300

    NewModificationOK

--- Bank-services/BankApplication/src/App.java
+++ Bank-services/BankApplication/src/App.java
@@ -3,7 +3,7 @@ import EJBModule.Response.BankClient;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // new ImplementationBankClientInformation().getInformation("235662");
+        new ImplementationBankClientInformation().getInformation("ID002");
         // new ImplementationBankClientInformation().insertAmount(new BankClient("4512365",4562));
     }
 }
--- GI-services/DisplayServer/DisplayServer/src/EJBModule/Response/BankClient.java
+++ GI-services/DisplayServer/DisplayServer/src/EJBModule/Response/BankClient.java
@@ -4,33 +4,46 @@ public class BankClient implements java.io.Serializable
 {
     private String cardId;
     private double amount;
+    private String currencyID ;
 
     public BankClient() {
     }
 
 
-    public BankClient(String cardId, double amount) 
+    public BankClient(String cardId, double amount)
     {
         setCardId(cardId);
         setAmount(amount);
     }
-    
-    
-    public String getCardId() 
+
+
+    public String getCardId()
     {
         return cardId;
     }
-    public void setCardId(String cardId) 
+    public void setCardId(String cardId)
     {
         this.cardId = cardId;
     }
-    public double getAmount() 
+    public double getAmount()
     {
         return amount;
     }
-    public void setAmount(double amount) 
+    public void setAmount(double amount)
     {
         this.amount = amount;
+    }
+
+
+    public String getCurrencyID()
+    {
+        return currencyID;
+    }
+
+
+    public void setCurrencyID(String currencyID)
+    {
+        this.currencyID = currencyID;
     }
 
 }
