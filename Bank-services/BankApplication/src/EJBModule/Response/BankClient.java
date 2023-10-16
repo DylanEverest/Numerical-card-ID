5b76fb64e91cda49338a0d9404392d65893783ca
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-16 09:56:06 +0300

    currencyID

--- Bank-services/BankApplication/src/EJBModule/Response/BankClient.java
+++ Bank-services/BankApplication/src/EJBModule/Response/BankClient.java
@@ -4,7 +4,7 @@ public class BankClient implements java.io.Serializable
 {
     private String cardId;
     private double amount;
-
+    private String currencyID ; 
     
     public BankClient() {
     }
@@ -32,6 +32,18 @@ public class BankClient implements java.io.Serializable
     public void setAmount(double amount) 
     {
         this.amount = amount;
+    }
+
+
+    public String getCurrencyID() 
+    {
+        return currencyID;
+    }
+
+
+    public void setCurrencyID(String currencyID) 
+    {
+        this.currencyID = currencyID;
     }
 
 }
