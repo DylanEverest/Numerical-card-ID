483666a9ce8f45e6a921f444107d29d92c0b01f7
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-16 10:25:30 +0300

    WebServiceFinished

--- Property-services/PropertyApplication/src/controller/PropertyController.java
+++ Property-services/PropertyApplication/src/controller/PropertyController.java
@@ -2,10 +2,14 @@ package controller;
 
 import databaseconnectivity.PgConnection;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import models.CurrencyTable;
 import models.PropertyView;
+import response.CurrencyResponse;
 import response.PropertyResponse;
 
 @Path("/property")
@@ -26,6 +30,22 @@ public class PropertyController
             babas[i] = new PropertyResponse(views[i].getPropertyId(), views[i].getAddress(), views[i].getPropertyPersonId(), views[i].getCardId(), views[i].getPurchaseDate() ,null);
         }
         return babas ;
+    }
+
+    @GET
+    @Path("/getCurrency")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CurrencyResponse getCurrencyNow() throws Exception
+    {
+        CurrencyTable cur=  CurrencyTable.getCurrencyNow(new PgConnection("property","postgres","","jdbc:postgresql://localhost:5432/").connectToDataBase(), false) ;
+
+        CurrencyResponse response = new CurrencyResponse();
+        
+        response.setPrecisionAriary(cur.getPrecisionAriary());
+        response.setPrecisionEuro(cur.getPrecisionEuro());
+        response.setPrecisionDollar(cur.getPrecisionDollar());
+
+        return response;
     }
 
 }
---
+++ Property-services/PropertyApplication/src/response/CurrencyResponse.java
@@ -0,0 +1,27 @@
+package response;
+
+public class CurrencyResponse 
+{
+    double precisionAriary;
+    double precisionEuro;
+    double precisionDollar;
+    
+    public double getPrecisionAriary() {
+        return precisionAriary;
+    }
+    public void setPrecisionAriary(double precisionAriary) {
+        this.precisionAriary = precisionAriary;
+    }
+    public double getPrecisionEuro() {
+        return precisionEuro;
+    }
+    public void setPrecisionEuro(double precisionEuro) {
+        this.precisionEuro = precisionEuro;
+    }
+    public double getPrecisionDollar() {
+        return precisionDollar;
+    }
+    public void setPrecisionDollar(double precisionDollar) {
+        this.precisionDollar = precisionDollar;
+    }    
+}
