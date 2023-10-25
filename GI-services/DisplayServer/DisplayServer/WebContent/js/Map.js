bf68a20d59cc4fd54b5f4083e3f3060715d47dd4
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-25 13:38:41 +0300

    modified:   GI-services/DisplayServer/DisplayServer/WebContent/Leaflet/Map.html 	modified:   GI-services/DisplayServer/DisplayServer/WebContent/css/app.css 	modified:   GI-services/DisplayServer/DisplayServer/WebContent/js/Map.js

--- GI-services/DisplayServer/DisplayServer/WebContent/js/Map.js
+++ GI-services/DisplayServer/DisplayServer/WebContent/js/Map.js
@@ -3,7 +3,7 @@ var map = L.map('map').setView([ -18.766947, 46.869107], 6.2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
 {
-    maxZoom: 1000,
+    maxZoom: 10000000,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 })
 .addTo(map);
@@ -15,8 +15,14 @@ var array_lglt =[];
 
 function addCoordinate(coupleLtLg) 
 {
+    console.log(coupleLtLg);
     array_lglt.push(coupleLtLg);
-    console.log(array_lglt);
+    L.marker([coupleLtLg.lat ,coupleLtLg.lng]).addTo(map) ;
+}
+
+function drawPolygon() 
+{
+
 }
 
 //     var marker = L.marker([51.5, -0.09]).addTo(map);
